12590ede74e8bf7f6703c45a4f06e3863c2986f3
olddon <davey.elliott@gmail.com>
2018-09-16 19:22:59 +0100

    Implemented function to reread data. Added update, deletion functions

--- psql_local.py
+++ psql_local.py
@@ -13,33 +13,39 @@ print("Connection established")
 
 cursor = conn.cursor()
 
+def reread_data():
+    cursor.execute("SELECT * FROM inventory")
+    rows = cursor.fetchall()
+    # Display the updated table
+    for row in rows:
+        print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+# reread_data()
+
 # drop previous table of same name if one exists
 cursor.execute("DROP TABLE IF EXISTS inventory;")
 print ("Finished dropping table (if existed)")
 
 # Create table
-cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER, weight REAL);")
 print ("Finished creating table")
 
-cursor.execute("insert into inventory (name, quantity) values (%s, %s);", ("banana", 150))
-cursor.execute("insert into inventory (name, quantity) values (%s, %s);", ("orange", 154))
-cursor.execute("insert into inventory (name, quantity) values (%s, %s);", ("apple", 100))
+# Update/Add to table
+cursor.execute("insert into inventory (name, quantity, weight) values (%s, %s, %s);", ("banana", 150, 34.1))
+cursor.execute("insert into inventory (name, quantity, weight) values (%s, %s, %s);", ("orange", 154, 110.41))
+cursor.execute("insert into inventory (name, quantity, weight) values (%s, %s, %s);", ("apple", 100, 60.18))
 print ("inserted 3 rows of data")
+reread_data()
 
-# # Drop previous table of same name if one exists
-# cursor.execute("CREATE TABLE inventory (name, quantity) VALUES  ("banana", 150);")
-# cursor.execute("INSERT INTO inventory (name, quantity) VALUES ("orange", 154);") 
-# cursor.execute("INSERT INTO inventory (name, quantity) VALUES  ("apple", 100);")
-# print ("Inserted 3 rows of data")
-
+# Update a data row in the table
 cursor.execute("UPDATE inventory SET quantity = %s WHERE name = %s;", (200, "banana"))
+print ("Updated 1 row of data")
+reread_data()
 
-# Get the updated values
-cursor.execute("SELECT * FROM inventory")
-rows = cursor.fetchall()
-# Display the updated table
-for row in rows:
-    print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+# Delete data row from table
+cursor.execute("DELETE FROM inventory WHERE name = %s;", ("orange",))
+print ("Deleted 1 row of data")
+reread_data()
+
 # Fetch all rows from table
 cursor.execute("SELECT * FROM inventory;")
 rows = cursor.fetchall()
